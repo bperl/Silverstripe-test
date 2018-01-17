995c339b703049aa4915d8c68426097ba8781cad
Zali Perlow <zali.perlow@flatironschool.com>
2018-01-17 14:28:29 -0500

    multiple templates

---
+++ mysite/code/HomePage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SilverStripe\Lessons;
+
+use Page;
+
+class HomePage extends Page
+
+{
+
+}
--- mysite/code/PageController.php
+++ mysite/code/PageController.php
@@ -1,7 +1,6 @@
 <?php
-
 use SilverStripe\CMS\Controllers\ContentController;
-
+use SilverStripe\View\Requirements;
 class PageController extends ContentController
 {
     /**
@@ -9,22 +8,31 @@ class PageController extends ContentController
      * permissions or conditions required to allow the user to access it.
      *
      * <code>
-     * [
+     * array (
      *     'action', // anyone can access this action
      *     'action' => true, // same as above
      *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
      *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-     * ];
+     * );
      * </code>
      *
      * @var array
      */
-    private static $allowed_actions = [];
-
+    private static $allowed_actions = array(
+    );
     protected function init()
     {
-        parent::init();
-        // You can include any CSS or JS required by your project here.
-        // See: https://docs.silverstripe.org/en/developer_guides/templates/requirements/
+      parent::init();
+  		Requirements::themedCSS("bootstrap.min");
+  		Requirements::themedCSS("style");
+  		Requirements::themedJavascript("common/modernizr");
+  		Requirements::themedJavascript("common/jquery-1.11.1.min");
+  		Requirements::themedJavascript("common/bootstrap.min");
+  		Requirements::themedJavascript("common/bootstrap-datepicker");
+  		Requirements::themedJavascript("common/chosen.min");
+  		Requirements::themedJavascript("common/bootstrap-checkbox");
+  		Requirements::themedJavascript("common/nice-scroll");
+  		Requirements::themedJavascript("common/jquery-browser");
+  		Requirements::themedJavascript("scripts");
     }
 }

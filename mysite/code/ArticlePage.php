7a16fbb4604e844ac1f575feec8a2bb6eba06d37
Zali Perlow <zali.perlow@flatironschool.com>
2018-01-18 14:41:43 -0500

    add custom fields

--- mysite/code/ArticlePage.php
+++ mysite/code/ArticlePage.php
@@ -2,9 +2,29 @@
 
 namespace SilverStripe\Lessons;
 
+use SilverStripe\Forms\DateField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
 use Page;
 
 class ArticlePage extends Page
 {
   private static $can_be_root = false;
+
+  private static $db = [
+    'Date' => 'Date',
+    'Teaser' => 'Text',
+    'Author' => 'Varchar',
+  ];
+
+  public function getCMSFields()
+{
+  $fields = parent::getCMSFields();
+  $fields->addFieldToTab('Root.Main', DateField::create('Date','Date of article'),'Content');
+  $fields->addFieldToTab('Root.Main', TextareaField::create('Teaser'),'Content');
+  $fields->addFieldToTab('Root.Main', TextField::create('Author','Author of article'),'Content');
+
+  return $fields;
+}
+
 }
